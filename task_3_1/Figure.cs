30a70e4e37dae3ecc391eefe1927dfa362535fa5
Syper_olao <olay1234538@gmail.com>
2020-12-12 01:54:29 +0300

    Добавьте файлы проекта.

---
+++ task_3_1/Figure.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3_1
+{
+    class Figure
+    {
+        PointF[] points;
+        float R;
+        Color color; 
+        RoundedConner roundedConner = new RoundedConner();
+
+        public void DrawFigure(Graphics graphics)
+        {
+            for (int i = 0, k = 1, z = 2; i < points.Length; i++, k++, z++)
+            {
+                roundedConner.DrawRoundedCorner(graphics, points[k], points[i], points[z], R, color);
+                if (k == points.Length - 1) k = -1;
+                if (z == points.Length - 1) z = -1;
+            }
+        }
+
+        public void setColor(Color color)
+        {
+            this.color = color; 
+        }
+
+        public void setRadious(float R)
+        {
+            this.R = R; 
+        }
+        
+        public void setPoints(PointF[] points)
+        {
+            this.points = points; 
+        }
+
+        public PointF[] GetPoints()
+        {
+            return points; 
+        }
+    }
+}
---
+++ task_3_1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_3_1
+{
+    public partial class Form1 : Form
+    {
+        PointF [] points;
+        Graphics graphics;
+        Figure[] figures;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            graphics = e.Graphics;
+            Draw(graphics);
+            
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+          
+            pictureBox1.Refresh();
+        }
+
+        private void Draw(Graphics graphics)
+        {
+            int n = int.Parse(textBox1.Text);
+            points = new PointF[n];
+            string[] temp_X = textBox2.Text.Split(' ');
+            string[] temp_Y = textBox3.Text.Split(' ');
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].X = float.Parse(temp_X[i]);
+                points[i].Y = float.Parse(temp_Y[i]);
+            }
+
+            float R = float.Parse(textBox4.Text.Replace(",", ".")); 
+           
+
+
+            Figure figures = new Figure();
+
+            figures.setRadious(R);
+            figures.setColor(Color.Red);
+            figures.setPoints(points);
+
+            figures.DrawFigure(graphics);
+     
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            graphics = CreateGraphics(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            graphics = CreateGraphics();
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+    }
+}
